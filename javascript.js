7a16be109e1b761d52b7f67226ddf8e5b82619c7
Darren Baker <d.baker089@gmail.com>
2022-08-26 12:50:49 -0400

    Simplify display

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-const bigScreen = document.querySelector('#lower-display')
-const smallScreen = document.querySelector('#upper-display');
+const display = document.querySelector('#display');
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
@@ -8,40 +7,25 @@ let firstNegative = false;
 let secondNegative = false;
 let firstDecimal = false;
 let secondDecimal = false;
+let selectNumber = false;
 
 
 const clickNumber = document.querySelectorAll('.number');
 
-// clickNumber.forEach(el => el.addEventListener('click', event => {
-//     if (operator === '' && result === '' && firstOperand.length < 10) {
-//         smallScreen.textContent += event.target.value;
-//         firstOperand += event.target.value;
-//     } else if (operator === '' && result !== '' && secondOperand.length < 10) {
-//         smallScreen.textContent = `${result}${operator}`;
-//         smallScreen.textContent += event.target.value;
-//         secondOperand += event.target.value;
-//     } else if (operator !== '' && result === '' && secondOperand.length < 10) {
-//         smallScreen.textContent += event.target.value;
-//         secondOperand += event.target.value;
-//     } else if (operator === '' && result !== '') {
-//         smallScreen.textContent = 'Choose an operator';
-//     } else {
-//         return;
-//     }
-// }));
-
 clickNumber.forEach(el => el.addEventListener('click', event => {
     if (operator === '' && result === '' && firstOperand.length < 10) {
-        smallScreen.textContent += event.target.value;
+        display.textContent += event.target.value;
         firstOperand += event.target.value;
+        clearNumber();
     } else if (operator !== '' && result === '' && secondOperand.length < 10) {
-        smallScreen.textContent += event.target.value;
         secondOperand += event.target.value;
+        display.textContent = `${secondOperand}`;
     } else if (operator === '' && result !== '' && secondOperand.length < 10) {
-        smallScreen.textContent = 'Choose an operator';
+        promptOperator();
     } else if (operator !== '' && result !== '' && secondOperand.length < 10) {
-        smallScreen.textContent += event.target.value;
         secondOperand += event.target.value;
+        display.textContent = `${secondOperand}`;
+        clearOperator();
     } else {
         return;
     }
@@ -51,17 +35,20 @@ const clickOperator = document.querySelectorAll('.operator');
 
 clickOperator.forEach(el => el.addEventListener('click', event => {
     if (firstOperand === '') {
-        smallScreen.textContent = 'Please choose a number';
+        promptNumber();
     } else if (operator === '' && result === '' && firstOperand !== '') {
-        smallScreen.textContent += event.target.value;
         operator = event.target.value;
+        event.target.classList.add('selected');
     } else if (operator !== '' && firstOperand !== '' && secondOperand !== 0) {
         return;
     } else {
-        smallScreen.textContent = result += event.target.value
         operator = event.target.value;
+        clearOperator();
+        event.target.classList.add('selected');
     }
 }));
+
+
 
 const clickDecimal = document.querySelector('#decimal');
 
@@ -69,16 +56,16 @@ clickDecimal.addEventListener("click", function () {
     if (decimal === true) {
         return;
     } else if (operator === '' && firstOperand === '') {
-        smallScreen.textContent += '0.';
+        display.textContent += '0.';
         firstOperand += '0.';
     } else if (operator === '' && firstOperand.length < 8) {
-        smallScreen.textContent += '.';
+        display.textContent += '.';
         firstOperand += '.';
     } else if (operator !== '' && secondOperand === '') {
-        smallScreen.textContent += '0.';
+        display.textContent += '0.';
         secondOperand += '0.';
     } else if (operator !== '' && secondOperand.length < 8) {
-        smallScreen.textContent += '.';
+        display.textContent += '.';
         secondOperand += '.';
     } else {
         return;
@@ -89,14 +76,14 @@ const clickDelete = document.querySelector('#delete');
 
 clickDelete.addEventListener("click", function () {
     if (operator === '' && result === '') {
-        smallScreen.textContent = firstOperand.slice(0, firstOperand.length - 1);
+        display.textContent = firstOperand.slice(0, firstOperand.length - 1);
         firstOperand = firstOperand.slice(0, firstOperand.length - 1);
-    } else if (operator !== '' && secondOperand === '') {
-        smallScreen.textContent = firstOperand;
-        operator = '';
     } else if (operator !== '' && secondOperand !== '') {
-        smallScreen.textContent = `${firstOperand}${operator}${secondOperand.slice(0, secondOperand.length - 1)}`
+        display.textContent = secondOperand.slice(0, secondOperand.length - 1);
         secondOperand = secondOperand.slice(0, secondOperand.length - 1);
+    } else if (operator !== '' && secondOperand === '') {
+        operator = '';
+        clearOperator();
     } else {
         return;
     }
@@ -113,27 +100,29 @@ clickClear.addEventListener('click', function () {
     secondNegative = false;
     firstDecimal = false;
     secondDecimal = false;
-    smallScreen.textContent = '';
-    bigScreen.textContent = '';
+    display.textContent = '';
+    clickOperator.forEach(function (element) {
+        element.classList.remove('selected');
+    });
 });
 
 const clickNegative = document.querySelector('#negative');
 
 clickNegative.addEventListener('click', function () {
     if (operator === '' && firstNegative === false) {
-        smallScreen.textContent = `-${firstOperand}`;
+        display.textContent = `-${firstOperand}`;
         firstOperand = `-${firstOperand}`;
         firstNegative = true;
     } else if (operator === '' && firstNegative === true) {
-        smallScreen.textContent = firstOperand.substring(1);
+        display.textContent = firstOperand.substring(1);
         firstOperand = firstOperand.substring(1);
         firstNegative = false;
     } else if (operator !== '' && secondNegative === false) {
-        smallScreen.textContent = `${firstOperand}${operator}-${secondOperand}`;
+        display.textContent = `${firstOperand}${operator}-${secondOperand}`;
         secondOperand = `-${secondOperand}`;
         secondNegative = true;
     } else if (operator !== '' && secondNegative === true) {
-        smallScreen.textContent = `${firstOperand}${operator}${secondOperand.substring(1)}`;
+        display.textContent = `${firstOperand}${operator}${secondOperand.substring(1)}`;
         secondOperand = secondOperand.substring(1);
         secondNegative = false;
     } else {
@@ -144,52 +133,47 @@ clickNegative.addEventListener('click', function () {
 const clickEquals = document.querySelector('#equals');
 
 clickEquals.addEventListener('click', function () {
-    if (operator === '' && firstOperand !== '') { bigScreen.textContent = firstOperand };
     if (operator !== '' && firstOperand !== '' && secondOperand !== '') {
         firstOperand = parseInt(firstOperand);
         secondOperand = parseInt(secondOperand);
         switch (operator) {
             case '+':
                 add(firstOperand, secondOperand);
-                smallScreen.textContent = '';
-                bigScreen.textContent = result;
+                display.textContent = result;
                 firstOperand = result;
                 operator = '';
                 secondOperand = '';
                 break;
             case '-':
                 subtract(firstOperand, secondOperand);
-                smallScreen.textContent = '';
-                bigScreen.textContent = result;
+                display.textContent = result;
                 firstOperand = result;
                 operator = '';
                 secondOperand = '';
                 break;
             case '*':
                 multiply(firstOperand, secondOperand);
-                smallScreen.textContent = '';
-                bigScreen.textContent = result;
+                display.textContent = result;
                 firstOperand = result;
                 operator = '';
                 secondOperand = '';
                 break;
             case '/':
                 divide(firstOperand, secondOperand);
-                smallScreen.textContent = '';
-                bigScreen.textContent = result;
+                display.textContent = result;
                 firstOperand = result;
                 operator = '';
                 secondOperand = '';
                 break;
             case '**':
                 exponent(firstOperand, secondOperand);
-                smallScreen.textContent = '';
-                bigScreen.textContent = result;
+                display.textContent = result;
                 firstOperand = result;
                 operator = '';
                 secondOperand = '';
                 break;
         }
+        clearOperator();
     } else {
         return;
     }
@@ -199,23 +183,49 @@ clickEquals.addEventListener('click', function () {
 // Operator Functions
 
 function add(a, b) {
-    result = a + b;
+    result = (a + b).toPrecision(5);
 };
 
 function subtract(a, b) {
-    result = a - b;
+    result = (a - b).toPrecision(5);
 };
 
 function multiply(a, b) {
-    result = a * b;
+    result = (a * b).toPrecision(5);
 };
 
 function divide(a, b) {
-    result = a / b;
+    result = (a / b).toPrecision(5);
 };
 
 function exponent(a, b) {
-    result = a ** b;
+    result = (a ** b).toPrecision(3);
+};
+
+// visual functions
+
+function promptOperator() {
+    clickOperator.forEach(function (element) {
+        element.classList.add('selected');
+    });
+};
+
+function clearOperator() {
+    clickOperator.forEach(function (element) {
+        element.classList.remove('selected');
+    });
+};
+
+function promptNumber() {
+    clickNumber.forEach(function (element) {
+        element.classList.add('selected');
+    });
+};
+
+function clearNumber() {
+    clickNumber.forEach(function (element) {
+        element.classList.remove('selected');
+    });
 };
 
 
@@ -223,11 +233,7 @@ function exponent(a, b) {
 //
 // keystroke functionality
 //
-// CSS: make display 1 line, dont need big and small displays
-// https://mrbuddh4.github.io/calculator/
-// 
-// CSS: make the operator an active state button
-// 
 // fix decimals (only input 1)
 // 
-// round?
+// clickNegative function
+//
